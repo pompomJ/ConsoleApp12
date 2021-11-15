fca9e25b4c77c2a24d3fcb9c74b350402491401f
ZamaVerce2 <Verce@LAPTOP-8JD0V3Q5>
2021-11-15 12:58:44 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //タプル型の戻り値
+            (string name, int age) GetMember()
+            {
+                var n = "Wings";
+                var a = 18;
+                return (n, a); //タプル型のリテラル
+            }
+            var m = GetMember();
+            Console.WriteLine(m.name); //出力値: Wings
+            Console.WriteLine(m.age); //出力値: 18
+
+        }
+    }
+}
