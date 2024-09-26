30993903de0c804494553c50ab7750197dc583a0
pompomJ <67637461+pompomJ@users.noreply.github.com>
2024-09-27 08:57:39 +0900

    Update Program.cs

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -9,12 +9,12 @@ namespace ConsoleApp12
             //タプル型の戻り値
             (string name, int age) GetMember()
             {
-                var n = "Wings";
+                var n = "Queens";
                 var a = 18;
                 return (n, a); //タプル型のリテラル
             }
             var m = GetMember();
-            Console.WriteLine(m.name); //出力値: Wings
+            Console.WriteLine(m.name); //出力値: Queens
             Console.WriteLine(m.age); //出力値: 18
 
         }
